7621e117264a948583d23808c99809b103ee4929
Anshu Kumar Rai <anshurai605@gmail.com>
2026-02-18 20:20:55 +0530

    created database table and landing page

--- components/header.jsx
+++ components/header.jsx
@@ -1,8 +1,23 @@
+"use client";
+
+
 import Image from "next/image";
 import React from "react";
 import Link from "next/link";
+import { useState } from "react";
+import {  SignInButton, UserButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
+import { Authenticated, Unauthenticated } from "convex/react";
+import { BarLoader } from "react-spinners";
+import useStoreUser from "../hooks/use-store-user";
+import { Plus, Ticket } from "lucide-react";
 
 const Header = () => {
+
+  const { isLoading } = useStoreUser();
+
+  const [showUpgradeModal, setShowUpgradeModal] = useState(false);
+
   return (
     <>
       <nav className="fixed top-0 left-0 right-0 bg-background/80 backdrop-blur-xl z-20 border-b ">
@@ -24,9 +39,57 @@ const Header = () => {
           {/* search and location --desktop only */}
 
           {/* right side actions */}
+          <div className="flex items-center">
+               {/* create event  */}
+                <Button variant="ghost" className="mr-2" size="sm" onClick={() => setShowUpgradeModal(true)}  >
+                  Pricing
+                </Button>
+                <Button variant="ghost" className="mr-2" size="sm" >
+                  <Link href="explore">Explore</Link>
+                </Button>
+            <Authenticated>
+              <Button size="sm" asChild className="flex gap-2 mr-4">
+                <Link href="/create-event">
+                <Plus className="w-4 h-4"/>
+                <span className="hidden sm:inline">Create Event</span>
+                </Link>
+              </Button>
+             
+                <UserButton >
+                  <UserButton.MenuItems>
+                    <UserButton.Link 
+                    label="My Tickets"
+                    labelIcon={<Ticket size={16} />}
+                    href="/my-tickets"
+                    />
+                    <UserButton.Link 
+                    label="My Events"
+                    labelIcon={<Ticket size={16} />}
+                    href="/my-events"
+                    />
+                  </UserButton.MenuItems>
+                </UserButton>
+              </Authenticated>
+
+             <Unauthenticated>
+                <SignInButton mode="modal">
+
+ <Button size="sm" className="mr-2" >Sign In</Button>
+                </SignInButton>
+              </Unauthenticated>
+               
+          </div>
         </div>
 
         {/* mobile search and location  */}
+
+        {/* loader */}
+
+        {isLoading && (
+          <div className="absolute bottom-0 left-0 w-full">
+            <BarLoader width={"100%"} color="#575757" />
+          </div>
+        )}
       </nav>
 
       {/* modals  */}
